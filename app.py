fdaede5a0c5fc41fe1a6a645ab7e0cb809d10e62
Armaan Mishra <armaanmishra48@gmail.com>
2023-10-23 08:34:14 +0530

    cors test

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://hdp2.vercel.app/"])
 limiter = Limiter(
     get_remote_address,
     app=app,
